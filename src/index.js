223127e8af242816ec6688760e5991cc46cf4fa4
nik4un <nik4un@yandex.ru>
2017-11-21 18:27:54 +0300

    feature (ndex.js) function of gameplay

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import readlineSync from 'readline-sync';
 
-const welcome = () => {
+const replayNumber = 3;
+
+export const welcome = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}! \n`);
   return userName;
 };
 
-welcome();
+export const gameProcess = (game, rule, userName) => {
+  console.log(`${rule}`);
+  const replayGame = (count) => {
+    const gameState = game();
+    console.log(`Question: ${gameState.quest}`);
+    const playerResponse = readlineSync.question('Your answer: ');
+
+    if (gameState.correctAnswer === playerResponse.toLowerCase()) {
+      console.log('Correct!');
+      if (count === 1) {
+        console.log(`Congratulations, ${userName}! \n`);
+        return;
+      }
+      replayGame(count - 1);
+    } else {
+      console.log(`«${playerResponse}» is the wrong answer. The correct answer is «${gameState.correctAnswer}». Let's try again, ${userName}!\n`);
+    }
+  };
+  return replayGame(replayNumber);
+};
