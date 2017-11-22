768032d7c78a7495a7abaa2c57f4da2d77daa8f8
nik4un <nik4un@yandex.ru>
2017-11-22 13:13:22 +0300

    feature (calc.js, brain-calc.js, brain-games.js) add game calc

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 
 import { welcome } from '..';
-import evenBrainGame from '../games/even';
 import calcBrainGame from '../games/calc';
 
 const userName = welcome();
-evenBrainGame(userName);
 calcBrainGame(userName);
---
+++ src/games/calc.js
@@ -0,0 +1,34 @@
+import { getRandomInt } from '../games-math';
+import { gameProcess } from '..';
+
+const rule = 'What is the result of the expression?';
+
+const calcGame = () => {
+  const exprArgFirst = getRandomInt(1, 12);
+  const exprArgSecond = getRandomInt(1, 12);
+  let quest;
+  let correctAnswer;
+  switch (getRandomInt(1, 4)) {
+    case 1: {
+      quest = `${exprArgFirst} + ${exprArgSecond}`;
+      correctAnswer = (exprArgFirst + exprArgSecond).toString();
+      break;
+    }
+    case 2: {
+      quest = `${exprArgFirst} - ${exprArgSecond}`;
+      correctAnswer = (exprArgFirst - exprArgSecond).toString();
+      break;
+    }
+    case 3: {
+      quest = `${exprArgFirst} * ${exprArgSecond}`;
+      correctAnswer = (exprArgFirst * exprArgSecond).toString();
+      break;
+    }
+    default: console.log('strange error');
+  }
+  return { quest, correctAnswer };
+};
+
+const calcBrainGame = userName => gameProcess(calcGame, rule, userName);
+export default calcBrainGame;
+// gameProcess(calcGame, rule, 'Nik');
